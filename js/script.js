e95fb2611ccbc26c5b6cb201e7cc5a2a037261c4
shareestark <shareegstark@gmail.com>
2024-04-23 11:54:30 -0400

    Adding list of repos on page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const overview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 
 const username = "shareestark";
 
@@ -24,5 +25,20 @@ const displayGitHubData = function (data) {
     <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
   </div>`;
   overview.append(newDiv);
+  getRepos();
 }; 
 
+const getRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+      const repoItem = document.createElement("li");
+      repoItem.classList.add("repo");
+      repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+      repoList.append(repoItem);
+    }
+  };
